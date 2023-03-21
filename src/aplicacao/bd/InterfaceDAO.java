febe57f758758ccf4999be85d7cd3fb48464322c
Bielmelo6 <gabrielmelodemenezes@gmail.com>
2023-03-21 20:12:55 -0300

    Interface usada para criação das funções utilizadas no banco de dados

---
+++ src/aplicacao/bd/InterfaceDAO.java
@@ -0,0 +1,12 @@
+package aplicacao.bd;
+
+import java.util.List;
+
+public interface InterfaceDAO<T> {
+
+    public void adiciona(T referencia);
+    public void remove(T referencia);
+    public List<T> busca();
+    public void atualiza(T referencia);
+
+}
